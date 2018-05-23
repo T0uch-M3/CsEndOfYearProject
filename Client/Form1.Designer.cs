cb14937381587ea1cbb8ca3c4631e50b73c91ab3
Unknown <adolf.hazmi@yahoo.com>
2018-05-23 03:40:00 +0100

    Almost done (for presentation)

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.confirm_btn = new System.Windows.Forms.Button();
+            this.tbPwd = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(205, 35);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(75, 20);
+            this.txtPort.TabIndex = 9;
+            this.txtPort.Text = "8910";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Port";
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(78, 35);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(75, 20);
+            this.txtIp.TabIndex = 7;
+            this.txtIp.Text = "192.168.1.147";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Host";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(305, 35);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 10;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(22, 80);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(358, 63);
+            this.txtMessage.TabIndex = 11;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(305, 154);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 12;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(305, 303);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 14;
+            this.btnTest.Text = "test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Location = new System.Drawing.Point(22, 154);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(152, 20);
+            this.tbLogin.TabIndex = 15;
+            // 
+            // confirm_btn
+            // 
+            this.confirm_btn.Location = new System.Drawing.Point(205, 185);
+            this.confirm_btn.Name = "confirm_btn";
+            this.confirm_btn.Size = new System.Drawing.Size(75, 23);
+            this.confirm_btn.TabIndex = 16;
+            this.confirm_btn.Text = "confirm";
+            this.confirm_btn.UseVisualStyleBackColor = true;
+            this.confirm_btn.Click += new System.EventHandler(this.confirm_btn_Click);
+            // 
+            // tbPwd
+            // 
+            this.tbPwd.Location = new System.Drawing.Point(22, 185);
+            this.tbPwd.Name = "tbPwd";
+            this.tbPwd.Size = new System.Drawing.Size(152, 20);
+            this.tbPwd.TabIndex = 17;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.ForeColor = System.Drawing.Color.Black;
+            this.txtStatus.HideSelection = false;
+            this.txtStatus.Location = new System.Drawing.Point(22, 341);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ReadOnly = true;
+            this.txtStatus.Size = new System.Drawing.Size(329, 130);
+            this.txtStatus.TabIndex = 18;
+            this.txtStatus.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 503);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.tbPwd);
+            this.Controls.Add(this.confirm_btn);
+            this.Controls.Add(this.tbLogin);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.Button confirm_btn;
+        private System.Windows.Forms.TextBox tbPwd;
+        private System.Windows.Forms.RichTextBox txtStatus;
+    }
+}
+
---
+++ Client/Form1.cs
@@ -0,0 +1,230 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form//CCCCCCCCCCCCCCCCCCCCLIENT
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SimpleTcpClient client;
+        byte[] data = new byte[1024];
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            client.Connect(txtIp.Text.ToString(), Int32.Parse(txtPort.Text));
+            btnConnect.Enabled = false;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new SimpleTcpClient();
+            client.StringEncoder = Encoding.UTF8;
+            client.DataReceived += Client_DataRecieved;
+           
+        }
+
+        private void Client_DataRecieved(object sender, SimpleTCP.Message e)
+        {
+            txtStatus.Invoke((MethodInvoker)delegate(){
+                txtStatus.Clear();
+                txtStatus.AppendText(e.MessageString);
+                //txtStatus.Text = txtStatus.Text.Insert(0, e.MessageString + "\n");
+            });
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            String s = "/M" + txtMessage.Text;
+            sendMsg(s, client.TcpClient);
+              //client.WriteLineAndGetReply(txtMessage.Text, TimeSpan.FromSeconds(3));
+              //client.Write(txtMessage.Text);
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            ipRange(IPAddress.Parse("192.168.1.147"));
+
+            //foreach (var c in IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpConnections())
+            //{
+            //    Console.WriteLine(c.State);
+            //    Console.WriteLine(c.LocalEndPoint);
+            //}
+
+
+
+            //foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            //{
+            //    foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+            //    {
+            //        if (!ip.IsDnsEligible)
+            //        {
+            //            if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            //            {
+            //                Console.WriteLine(ip.Address.ToString());
+            //            }
+            //        }
+            //    }
+            //}
+
+
+
+            //System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
+            //System.Net.NetworkInformation.PingReply rep = p.Send("192.168.1.2");
+            //if (rep.Status == System.Net.NetworkInformation.IPStatus.Success)
+            //{
+            //    Console.WriteLine("working");
+            //}
+            //else
+            //    Console.WriteLine("not wroking");
+
+            //String strHostName = Dns.GetHostName();
+            //IPHostEntry iphostentry = Dns.GetHostEntry(strHostName);
+            //IPAddress[] allIp = Dns.GetHostAddresses(strHostName);
+            //List<IPAddress> list = allIp.ToList();
+
+            //foreach (IPAddress ipaddress in iphostentry.AddressList)
+            //{
+            //    Console.WriteLine(ipaddress);
+            //}
+
+            //list.ForEach(Console.WriteLine);
+           
+        }
+
+
+        public void ipRange(IPAddress ip)
+        {
+            String newIp = null, newIp2 = null;
+            int countDots = 0;
+
+            Ping ping = new Ping();
+            PingReply pingReply = ping.Send("192.168.1.147");
+
+            if (pingReply.Status == IPStatus.Success)
+            {
+                
+                System.Console.WriteLine("good");
+                
+            }
+            else
+                System.Console.WriteLine("bad");
+            Ping sender  = new Ping();
+            sender.PingCompleted += new PingCompletedEventHandler(PingCompletedCallback);//method below
+            //sender.SendAsync(dest, timeout, buffer, options, textLabel);
+
+
+
+
+            //try
+            //{
+            //    IAsyncResult ar = (IAsyncResult)client.Connect("192.168.1.148", Int32.Parse(txtPort.Text));
+            //    client.TcpClient.Close();
+
+            //}
+
+
+            //catch (SocketException e)
+            //{
+            //    System.Console.WriteLine("Fucked connectin");
+            //}
+
+            //************************************* waiting testing, check if the ip adrs is the other machine adr or no***************
+            //IPGlobalProperties ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+            //IPEndPoint[] ipEndPoints = ipProperties.GetActiveTcpListeners();
+
+
+            //foreach (IPEndPoint endPoint in ipEndPoints)
+            //{
+            //    if (endPoint.Port == Int32.Parse(txtPort.Text))
+            //    {
+
+            //        System.Console.WriteLine("ip adr: " + endPoint.Address.ToString());
+            //        System.Console.WriteLine("in use");
+            //        break;
+            //    }
+            //}
+            //**************************************************************************************************************************
+
+
+
+
+
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            List<IPAddress> list = host.AddressList.ToList();
+            IPAddress currentIp = list[3];
+            String stringIp = currentIp.ToString();
+            System.Console.WriteLine(stringIp);
+            for (int i = 0; i < stringIp.Length; i++)
+            {
+                if (stringIp[i].Equals('.'))
+                    countDots++;
+                if (countDots < 2)
+                    newIp += stringIp[i];
+                System.Console.WriteLine(newIp);
+            }
+            try { 
+                //client.Connect("192.168.1.148", Int32.Parse(txtPort.Text));
+                
+            }
+            catch (SocketException)
+            {
+
+            }
+        
+            //for (int i = 0; i < 255; i++)
+            //{
+            //    for (int j = 0; j < 255; j++)
+            //    {
+            //        newIp2 = newIp + "." + i + "." + j;
+            //        client.Connect(newIp2, Int32.Parse(txtPort.Text));
+                    
+            //        //System.Console.WriteLine(newIp2);
+            //    }
+            //}
+        }
+
+        private void PingCompletedCallback(object sender, PingCompletedEventArgs e)
+        {
+            if (e.Reply.Status == IPStatus.Success)
+            {
+                System.Console.WriteLine("ok");
+            }
+            else
+            {
+                System.Console.WriteLine("not ok");
+            }
+        }
+
+        private void confirm_btn_Click(object sender, EventArgs e)
+        {
+            //client.WriteLine("/L"+tbLogin.Text);
+            String s = "/L" + tbLogin.Text;
+            sendMsg(s, client.TcpClient);
+            System.Threading.Thread.Sleep(500);
+            client.WriteLine("/P"+tbPwd.Text);
+        }
+
+        public void sendMsg(String s, TcpClient target)
+        {
+            string welcome = s;
+            data = Encoding.ASCII.GetBytes(welcome);
+            target.GetStream().Write(data, 0, data.Length);
+        }
+    }
+}
